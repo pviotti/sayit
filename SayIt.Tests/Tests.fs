692c6436234d9204f538254331a8570bee79d0a0
Paolo <pviotti@users.noreply.github.com>
2019-10-16 22:11:31 +0100

    improve Azure Pipelines configuration (#13)
    - use Dotnet Core tasks instead of VS*
    - build on multiple platforms
    - publish test result reports
    - publish test coverage reports

--- SayIt.Tests/Tests.fs
+++ SayIt.Tests/Tests.fs
@@ -9,12 +9,12 @@ module ``SayIt tests`` =
 
     type SayItTests () =
 
-        [<Fact>] 
+        [<Fact>]
         member __.``when asked for version it should print it and exit with 0`` () =
             use sw = new StringWriter()
             Console.SetOut(sw)
             SayIt.Program.main([|"--version"|]) |> should equal 0
-            sw.ToString() |> should equal (sprintf "sayit version %s\n" SayIt.Config.VERSION)
+            sw.ToString().Trim() |> should equal (sprintf "sayit version %s" SayIt.Config.VERSION)
             Console.SetOut(new StreamWriter(Console.OpenStandardOutput()))
 
 
