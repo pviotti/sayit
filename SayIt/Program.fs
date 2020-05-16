7907a8ae40a6c3ffb2bb61b1865cfcc98d58996e
Paolo <pviotti@users.noreply.github.com>
2020-05-16 21:37:27 +0100

    read input from stdin if missing as parameter

--- SayIt/Program.fs
+++ SayIt/Program.fs
@@ -4,6 +4,8 @@ open SayIt.Config
 open SayIt.Voices
 open SayIt.Formats
 
+open Argu
+open System
 open System.Threading.Tasks
 open Microsoft.CognitiveServices.Speech
 open Microsoft.CognitiveServices.Speech.Audio
@@ -25,28 +27,40 @@ let handleSynthesisResult (task: Task<SpeechSynthesisResult>) =
         1
     | _ -> 0
 
-let performSpeechSynthesis (config: Argu.ParseResults<Args>, speechConfig: SpeechConfig) =
+let performSpeechSynthesis (config: Argu.ParseResults<Args>, speechConfig: SpeechConfig, input: string) =
     if config.Contains Output then
         let output = config.GetResult Output
-        let outputFormat = getFormatId( config.PostProcessResult (Format, FormatType.FromString))
+        let outputFormat = getFormatId (config.PostProcessResult(Format, FormatType.FromString))
         speechConfig.SetSpeechSynthesisOutputFormat(outputFormat)
         use fileOutput = AudioConfig.FromWavFileOutput(output)
         use synthetizer = new SpeechSynthesizer(speechConfig, fileOutput)
-        handleSynthesisResult (synthetizer.SpeakTextAsync(config.GetResult Input))
+        handleSynthesisResult (synthetizer.SpeakTextAsync(input))
     else
         use synthetizer = new SpeechSynthesizer(speechConfig)
-        handleSynthesisResult (synthetizer.SpeakTextAsync(config.GetResult Input))
+        handleSynthesisResult (synthetizer.SpeakTextAsync(input))
 
 [<EntryPoint>]
 let main argv =
     match getConfiguration (argv) with
     | Config config ->
-         let key = config.GetResult Key
-         let region = config.GetResult Region
-         let voice = getVoiceId (config.PostProcessResult (Voice, VoiceType.FromString))
 
-         let speechConfig = SpeechConfig.FromSubscription(key, region)
-         speechConfig.SpeechSynthesisVoiceName <- voice
+        let input =
+            if config.Contains Input then
+                config.GetResult Input
+            elif Console.IsInputRedirected then
+                let mutable input = String.Empty
+                while Console.In.Peek() <> -1 do
+                    input <- input + Console.ReadLine()
+                input
+            else
+                config.Raise "ERROR: missing argument '<input>'." ErrorCode.CommandLine true
 
-         performSpeechSynthesis (config, speechConfig)
+        let key = config.GetResult Key
+        let region = config.GetResult Region
+        let voice = getVoiceId (config.PostProcessResult(Voice, VoiceType.FromString))
+
+        let speechConfig = SpeechConfig.FromSubscription(key, region)
+        speechConfig.SpeechSynthesisVoiceName <- voice
+
+        performSpeechSynthesis (config, speechConfig, input)
     | ReturnVal ret -> ret
