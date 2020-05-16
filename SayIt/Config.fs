fe54c21c4c392eda6e2af2c2a67629eb1ad59346
Paolo <pviotti@users.noreply.github.com>
2020-05-16 21:40:51 +0100

    simplify usage message

--- SayIt/Config.fs
+++ SayIt/Config.fs
@@ -30,15 +30,16 @@ type Args =
         member s.Usage =
             match s with
             | Version _ -> "print sayit version"
-            | Setup _ -> "setup the configuration file"            
-            | List_Voices _ -> "list available voice shorthands, with their corresponding voice ids"
-            | List_Formats _ -> "list available output format shorthands, with their corresponding output format ids"
-            | Voice _ -> "the voice shorthand, which maps to one of the available voice ids (see `sayit -lv` for details)"
-            | Format _ -> "the format shorthand of the audio output, which maps to one fo the available format ids (see `sayit -lf` for details)"
+            | Setup _ -> "setup the configuration file"
+            | List_Voices _ -> "list the available voice shorthands with their corresponding voice ids"
+            | List_Formats _ -> "list the available output format shorthands with their corresponding output format ids"
+            | Voice _ -> "the voice shorthand"
+            | Format _ -> "the audio output format shorthand"
             | Output _ -> "the path of the output file"
-            | Input _ -> "the text to be pronounced"
+            | Input _ -> "the text to be pronounced (if missing, " + PROGRAM_NAME + " will try to read it from stdin)"
             | Key _ -> "the subscription key of your Azure Cognitive Services resource"
-            | Region _ -> "the region identifier of your Azure Cognitive Services resource (see: https://aka.ms/speech/sdkregion)"
+            | Region _ ->
+                "the region identifier of your Azure Cognitive Services resource (see: https://aka.ms/speech/sdkregion)"
 
 type ConfigOrInt =
     | Config of Argu.ParseResults<Args>
